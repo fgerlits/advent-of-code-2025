d33e587830cb03e5a7f4fca5bd6a7678823c5b77
Ferenc Gerlits <fgerlits@gmail.com>
2025-12-08 08:24:32 +0100

    refactoring

---
+++ 8/common.rb
@@ -0,0 +1,34 @@
+class Partition
+    def initialize(size)
+        @leaders = size.times.to_a
+    end
+
+    def add_connection(a, b)
+        raise unless a < b
+        new, old = [@leaders[a], @leaders[b]].sort
+        if new != old
+            @leaders.map!{|leader| if leader == old then new else leader end}
+        end
+    end
+
+    def sizes()
+        @leaders.group_by(&:itself).map{|_, group| group.size}
+    end
+end
+
+def parse(stream)
+    stream.readlines.map{|line| line.chomp.split(',').map(&:to_i)}
+end
+
+def distance_squared(p, q)
+    p.zip(q).map{|a, b| (a - b) ** 2}.sum
+end
+
+def compute_distances(points)
+    n = points.size
+    (0...n).flat_map do |i|
+        (i+1...n).map do |j|
+            [distance_squared(points[i], points[j]), i, j]
+        end
+    end
+end
